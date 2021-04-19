20da6cecd6252abd8919a73a337a2bb6770ef31e
FilippoGuarda <filippo.guarda96@gmail.com>
2021-04-19 14:18:24 +0200

    more work done on background definition

--- motion-detection.py
+++ motion-detection.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 import time 
-import random
 
 
 
@@ -37,6 +36,25 @@ bookDetectorParameters.filterByInertia = False
 detector_person = cv2.SimpleBlobDetector_create(personDetectorParameters)
 detector_book = cv2.SimpleBlobDetector_create(bookDetectorParameters)
 
+
+
+def skip_background(contours, frame, original_contour, shift1, shift2, index, thresh = 30):
+    #ignore contours that are part of the background
+    
+    
+    cv2.drawContours(original_contour, contours, index, 255, -1)
+
+    # take two shifted contours, add them and mask using original contours to obtain internal contour
+    cv2.drawContours(shift1, contours, index, 255, 10, offset=(5,0))
+    cv2.drawContours(shift2, contours, index, 255, 10, offset=(-5,0))
+    internal_contour = cv2.bitwise_or(shift1, shift2, mask = original_contour)
+    # get external contour
+    external_contour = cv2.bitwise_or(shift1, shift2, mask = 255 - original_contour)
+    external_median = np.median(frame[external_contour > 0], overwrite_input=True)
+    internal_median = np.median(frame[internal_contour > 0], overwrite_input=True)
+    if np.abs(external_median - internal_median) < thresh:
+        return True
+    
 def object_detector(contours, index, mask, color_mask): 
     #detect, classify and log objects moving in the frame
     
@@ -51,12 +69,8 @@ def object_detector(contours, index, mask, color_mask):
 
 # define background subtraction method
 # fgbg = cv2.createBackgroundSubtractorMOG2(history = 500, varThreshold = 50, detectShadows=False)
-fgbg = cv2.createBackgroundSubtractorKNN(history = 150, dist2Threshold = 150, detectShadows=False)
+fgbg = cv2.createBackgroundSubtractorKNN(history = 150, dist2Threshold = 200, detectShadows=True)
 
-def random_color():
-    rgbl=[255,0,0]
-    random.shuffle(rgbl)
-    return tuple(rgbl)
 
 
 def change_detection(video_path):
@@ -74,43 +88,38 @@ def change_detection(video_path):
             break
 
         mask = fgbg.apply(frame)
-        # mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-        # cv2.imshow('mask', mask)
-        blur=cv2.GaussianBlur(mask,(11,11),0)
-        # cv2.imshow('Blur', blur)
-        ret,thresh = cv2.threshold(blur,150,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        # cv2.imshow('thresh', thresh)
+        cv2.imshow('mask', mask)
+        blur=cv2.GaussianBlur(mask,(5,5),0)
+        cv2.imshow('Blur', blur)
+        ret,thresh = cv2.threshold(blur,50,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        cv2.imshow('thresh', thresh)
         
        
         # try with opening and closing of the binary image
         opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN,  cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3)), iterations = 1)
-        # cv2.imshow('opening', opening)
+        cv2.imshow('opening', opening)
         closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE,  cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(7,7)), iterations = 2)
-        # cv2.imshow('closing', closing)
-        eroded = cv2.erode(closing, (5, 5), iterations = 2)
+        cv2.imshow('closing', closing)
         
         # TODO: find a way to distinguish if blob was removed from backgound or was added to it
         contours, hierarchy = cv2.findContours(closing.astype(np.uint8)*255, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        eroded_contours, hierarchy = cv2.findContours(eroded.astype(np.uint8)*255, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         image_external = np.zeros(closing.shape, np.uint8)
         colored_contours = np.zeros(frame.shape)
+        original_contour = np.zeros(closing.shape, np.uint8)
+        shift2 = np.zeros(closing.shape, np.uint8)
+        shift1 = np.zeros(closing.shape, np.uint8)
         
         
         for i in range(len(contours)):
             
-            #ignore contours that are part of the background
-            original_contour = shifted_contour = np.zeros(closing.shape, np.uint8)
-            cv2.drawContours(original_contour, contours, i, 255, 3)
-            cv2.imshow('original', original_contour)
-            for j in range(len(eroded_contours)):
-                cv2.drawContours(shifted_contour, eroded_contours, j, 255, 3)
-                cv2.imshow('shifted', shifted_contour)
-                if 
-            
-            object_detector(contours, i, image_external, colored_contours)
-                
-        cv2.imshow('contours', image_external)
-        cv2.imshow('contours', colored_contours)
+            if skip_background(contours, frame, original_contour, shift1, shift2, i, 20):
+                continue
+            else:
+                object_detector(contours, i, image_external, colored_contours)
+        
+          
+        # cv2.imshow('contours', image_external)
+        # cv2.imshow('contours', colored_contours)
         
         #visualize masked image
         masked_image = np.copy(frame)
@@ -123,7 +132,7 @@ def change_detection(video_path):
         im_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         keypoints2 = detector_book.detect(image_external)
         im_keypoints2 = cv2.drawKeypoints(im_keypoints, keypoints2, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        cv2.imshow('Video',im_keypoints2)
+        # cv2.imshow('Video',im_keypoints2)
         time.sleep(0.02)
         if cv2.waitKey(1) == ord('q'):
                 break
